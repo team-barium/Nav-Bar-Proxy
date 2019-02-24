0bad8bfded872c205f3ef519150ec008f495ef44
Gabriella Pernama <gabypernama@gmail.com>
2019-02-24 14:25:32 -0800

    connected proxy server

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const port = 3005;
+const parser = require("body-parser");
+const axios = require("axios");
+
+let globalID = null;
+
+const app = express();
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+
+app.use(cors());
+app.post("/randomID", (req, resp) => {
+  globalID = Math.ceil(Math.random() * 19);
+  console.log(globalID);
+  resp.end();
+});
+app.use(express.static(path.resolve(__dirname, "./public")));
+
+app.post("/random/:id", (req, resp) => {
+  let { id } = req.params;
+  globalID = id;
+  resp.end();
+});
+
+app.get("/suggestions", (req, response) => {
+  axios
+    .get(`http://localhost:3004/suggestions/${globalID}`)
+    .then(res => {
+      response.status(200).send(res.data);
+    })
+    .catch(err => console.log(err));
+});
+
+app.get("/search/:keyword", (req, response) => {
+  let { keyword } = req.params;
+  axios
+    .get(`http://localhost:3001/search/${keyword}`)
+    .then(res => {
+      response.status(200).send(res.data);
+    })
+    .catch(err => console.log(err));
+});
+
+app.get("/reviews/", (req, res) => {
+  axios
+    .get(`http://localhost:3003/reviews/${globalID}`)
+    .then(({ data }) => res.send(JSON.stringify(data)))
+    .catch(err => res.send(err));
+});
+
+app.get("/reviews/stats", (req, res) => {
+  axios
+    .get(`http://localhost:3003/reviews/${globalID}/stats`)
+    .then(({ data }) => res.send(JSON.stringify(data)))
+    .catch(err => res.send(err));
+});
+
+app.listen(port, () => console.log("App is listening on port: ", port));
